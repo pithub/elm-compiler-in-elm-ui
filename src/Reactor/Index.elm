9e94bda38eee0b8c8923d6d54b6ea3ed2ba00cbe
Pit Capitain <pit@capitain.de>
2024-09-04 08:16:57 +0200

    Add command to show original license

--- src/Reactor/Index.elm
+++ src/Reactor/Index.elm
@@ -250,6 +250,9 @@ executeCommand command =
             IO.bind isHtmlEnabled <|
                 elmRepl (Terminal.Repl.Module (String.dropLeft 3 command)) Nothing
 
+          else if command == "l" then
+            showLicense
+
           else
             createFile command
         ]
@@ -315,8 +318,25 @@ m <mount> <target> - mount <mount> to <target>
 Other
 
 h - show help (this text)
+l - show original license
 p - show id prefix
 p <prefix> - set id prefix
+"""
+
+
+showLicense : IO a h ()
+showLicense =
+    Terminal.putLine """Copyright 2012-present Evan Czaplicki
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 
